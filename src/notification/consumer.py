627f53de1884f70dc24dfd0437908f0d3dda315c
yussaaa <yusa.li@mail.utoronto.ca>
2023-12-30 10:47:25 -0500

    Notification moudle initial commit

--- src/notification/consumer.py
+++ src/notification/consumer.py
@@ -1,30 +1,21 @@
 import pika, sys, os, time
-from pymongo import MongoClient
-import gridfs
-from convert import to_mp3
+from send import email
 
 
 def main():
-    client = MongoClient("host.minikube.internal", 27017)
-    db_videos = client.videos
-    db_mp3s = client.mp3s
-    # gridfs
-    fs_videos = gridfs.GridFS(db_videos)
-    fs_mp3s = gridfs.GridFS(db_mp3s)
-
     # rabbitmq connection
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
 
     def callback(ch, method, properties, body):
-        err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
+        err = email.notification(body)
         if err:
             ch.basic_nack(delivery_tag=method.delivery_tag)
         else:
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
     channel.basic_consume(
-        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
+        queue=os.environ.get("MP3_QUEUE"), on_message_callback=callback
     )
 
     print("Waiting for messages. To exit press CTRL+C")
